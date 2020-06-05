0e4d08b03b9649c309c990e9298efc330708f1bb
bibi hassan <bibihassan2000@gmail.com>
2020-06-05 20:13:41 +0000

    Part Two of Step 5: retrieve data from DB to display comments

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -17,6 +17,9 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,19 +27,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import com.google.gson.Gson;
-
-
+ 
+ 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
   
   private ArrayList<String> names = new ArrayList<String>();
-
+ 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String json = convertToJsonUsingGson(names);
+    Query query = new Query("Task").addSort("comment", SortDirection.DESCENDING);
+ 
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+ 
+    ArrayList<String> commentsFromDB = new ArrayList<String>();
+    for (Entity entity : results.asIterable()) {
+        String entityComment = (String) entity.getProperty("comment");
+        commentsFromDB.add(entityComment);
+    }
+ 
+    String json = convertToJsonUsingGson(commentsFromDB);
     response.setContentType("application/json;");
     response.getWriter().println(json);
+
   }
 
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
