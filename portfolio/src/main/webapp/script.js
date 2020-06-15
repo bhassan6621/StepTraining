ba4f60f28cceb1b7af1188bb3b5f3c76e988704f
bhassan6621 <31637947+bhassan6621@users.noreply.github.com>
2020-06-15 14:28:30 -0400

    Add an experience/ skills section to portfolio (with a pie chart) and a Places Visited section (with google maps) (#6)
    * added a chart that shows my experience and rearranged the html file
    
    * rewording previous commit to: deleted auto generated files
    
    * added a map feature with markers of places i've visted
    
    * cleaned up code for review
    
    * fixed pr comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,10 +13,13 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a greeting to the page.
  */
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(experienceChart);
 
-var count = 0;
+let count = 0;
+let map;
 
 function addRandomGreeting() {
   const greetings =
@@ -44,12 +47,67 @@ function addComment() {
       'Content-Type': 'application/json'},
       body: getUserInput()
     }).then(response => response.json()).then((stats) => {
-        const nameContainer = document.getElementById('name-container');
+        const nameContainer = document.getElementById('comment-container');
         nameContainer.innerHTML = "";
         for(let i = 0; i < stats.length; i++) {
             nameContainer.appendChild(
             createListElement(stats[i]));
         }
+    });
+}
+
+function experienceChart() {
+    const data = new google.visualization.DataTable();
+    data.addColumn('string', 'Language');
+    data.addColumn('number', 'Years');
+        data.addRows([
+            ['Python', 2],
+            ['Html/Css', 5],
+            ['Java', 4],
+            ['C++', 1],
+            ['JS', 1]
+        ]);
+
+    const options = {
+        'title': 'Experience',
+        'height': 550,
+        'is3D': true,
+        color: 'white',
+        backgroundColor: 'transparent',
+        legend : { position: 'bottom' }
+    };
+
+    const chart = new google.visualization.PieChart(
+        document.getElementById('chart-container'));
+    chart.draw(data, options);
+}
+
+function initMap() {
+    const map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 3, center: {lat: 2.8, lng: -187.3}});
+    
+    const NYMarker = new google.maps.Marker({
+        position: {lat: 40.77, lng: -73.97},
+        map: map,
+        title: 'Central Park'
+    });
+
+    const GYMarker = new google.maps.Marker({
+        position: {lat: 5.68, lng: -57.39},
+        map: map,
+        title: 'Guyana'
+    });
+    
+    const CAMarker = new google.maps.Marker({
+        position: {lat: 38.01, lng: -122.56},
+        map: map,
+        title: 'San Rafeal'
+    });
+
+     const UKMarker = new google.maps.Marker({
+        position: {lat: 53.51, lng: -2.31},
+        map: map,
+        title: 'Manchester, UK'
     });
 }
 
